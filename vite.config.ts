10a9cc5fa1c61e34be1c33f11ef7c8723788c080
gurdian0614 <ivan.gurdian@outlook.com>
2026-03-17 20:31:41 -0600

    Config tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +8,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  test: {
+    environment: 'jsdom', //Simula el navegador
+    globals: true, //Permite usar funciones globales como describe, it, expect sin importarlas
+    setupFiles: './setupTests.ts', //Archivo de configuración para pruebas, donde se pueden configurar cosas como el entorno de pruebas, mocks, etc.
+    reporters: 'verbose', //Muestra información detallada sobre cada prueba que se ejecuta, incluyendo el nombre de la prueba, el resultado (pasó o falló) y cualquier mensaje de error asociado.
+  }
 })
